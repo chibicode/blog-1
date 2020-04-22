6c85d2d328f14cc3afd6b3e9e808e15c1ddc9204
Shu Uesugi <shu@chibicode.com>
2020-04-22 14:35:12 -0700

    Update vercel.js

--- pages/ja/2020/vercel.js
+++ pages/ja/2020/vercel.js
@@ -117,7 +117,7 @@ export default withViews(({ views }) => (
       <em>
         <b>フロントエンド版のAWS</b>
       </em>
-      に化ける可能性がある」とも言ってくれました。
+      に化ける可能性がある」とも話していました。
     </P>
 
     <H2 id="megatrends">Three Megatrends for Frontend Developer Teams</H2>
